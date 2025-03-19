426a6db73c8d5dd145ac5e0584ff516f28757493
Yonatan Isakov <yonatan156354@gmail.com>
2025-03-19 06:43:13 +0200

    edit splash screen

--- app/src/main/java/com/idz/teamup/MainActivity.kt
+++ app/src/main/java/com/idz/teamup/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
+         installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
